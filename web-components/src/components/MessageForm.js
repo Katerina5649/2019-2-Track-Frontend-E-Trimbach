2258957facbd20b181626ebb3bcc7f4d799c7c40
Trimbach Ekaterina <trimbachkatya@gmail.com>
2019-10-14 22:19:32 +0300

    Just for save changes

--- web-components/src/components/MessageForm.js
+++ web-components/src/components/MessageForm.js
@@ -4,66 +4,107 @@ const template = document.createElement('template');
 // language=HTML
 template.innerHTML = `
     <style>
-        form-input {
-            width: 100%;
+        #messList{
+            list-style-type: decimal;
+            overflow-y: scroll;
+            height: 700px;
+            bottom: 0;
+            text-align: right;
+            text-align: right;
+            border-collapse: separate;
+            margin-bottom: 10px;
         }
         
-        .result {
-            color: red;
+        form-input {
+            height: 6vh;
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
         }
         
         input[type=submit] {
             visibility: collapse;
         }
+        
     </style>
-    <ol id="oll">
-        <li>0</li>
-        <li>1</li>
-        <li>2</li>
+    <ol id="messList">
     </ol>
-    <script>
-        var olNode = document.getElementById("oll")
-        olNode.append('append')
-        olNode.before("before")
-        olNode.after("after")
-        
-        console.log(olNode)
-        
-        olNode.appendChild("aaa")
-        document.body.appendChild(olNode)
-        document.body.append(olNode)
-    </script>
-
     <form>
-        <div class="result"></div>
         <form-input name="message-text" placeholder="Введите сообщение"></form-input>
     </form>
-
+    <script>
+        doSomething()
+    </script>
    
 `;
 
 class MessageForm extends HTMLElement {
+
+
     constructor () {
-        super();
+      super();
+      this._shadowRoot = this.attachShadow({mode: 'open'});
+      this._shadowRoot.appendChild(template.content.cloneNode(true));
+      this.$form = this._shadowRoot.querySelector('form');
+      this.$input = this._shadowRoot.querySelector('form-input');
+      //поиск по id
+      this.$messList = this._shadowRoot.querySelector("#messList");
+
+        //получаю Лист сообщений с localStorage
+      this.messageList = MessageForm.getElementFromLocalStorage("messList");
+      if (this.messageList == null)
+        //todo
+        this.messageList  = []
+      //отображение списка messageList
+
+        for (i = 0; i < this.messageList.length; i++) {
+          const message = document.createElement("div")
+          message.innerHTML =this.messageList[i]
+          message.className = "mess"
+         // message.style.top = "100 px";
+          this.$messList.append(message)
+        }
+
+      //Прокручиваю сообщения вниз
+      this.$messList.scrollTo(0, this.$messList.scrollHeight)
 
 
-
-        this._shadowRoot = this.attachShadow({ mode: 'open' });
-        this._shadowRoot.appendChild(template.content.cloneNode(true));
-        this.$form = this._shadowRoot.querySelector('form');
-        this.$input = this._shadowRoot.querySelector('form-input');
-        this.$message = this._shadowRoot.querySelector('.result');
-
-        this.$form.addEventListener('submit', this._onSubmit.bind(this));
-        this.$form.addEventListener('keypress', this._onKeyPress.bind(this));
+      //Добавление listener
+      this.$form.addEventListener('submit', this._onSubmit.bind(this));
+      this.$form.addEventListener('keypress', this._onKeyPress.bind(this));
 
     }
 
     _onSubmit (event) {
+        //todo что это?
         event.preventDefault();
-        this.$message.innerText = this.$input.value;
+        //добавление сообщения в html
+        const message = document.createElement("div")
+        message.className = "mess"
+
+        message.innerHTML = this.$input.value
+
+      //вставляет вверх списка
+        this.$messList.append(message)
+
+        //добавление сообщения в list
+        this.messageList.push(this.$input.value)
+        MessageForm.addElemToLocalStorage("messList", this.messageList )
         this.$input.clearInput()
 
+        this.$messList.scrollTo(0, this.$messList.scrollHeight)
+
+    }
+
+
+    static addElemToLocalStorage (key , val){
+       const jsonObj = JSON.stringify(val)
+      localStorage.setItem(key, jsonObj)
+    }
+
+    static getElementFromLocalStorage(key){
+      console.log(JSON.parse(localStorage.getItem(key)))
+      return JSON.parse(localStorage.getItem(key))
     }
 
     _onKeyPress (event) {
@@ -71,8 +112,6 @@ class MessageForm extends HTMLElement {
             this.$form.dispatchEvent(new Event('submit'));
         }
     }
-
-
 }
 
 customElements.define('message-form', MessageForm);
